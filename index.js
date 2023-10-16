556167b2bea4ab295242a49aa6eb8ddc1a306e23
Stephen Mohler <7486steve@gmail.com>
2023-10-16 12:50:45 -0700

    add draw, reset, resize, and eraser functions

--- index.js
+++ index.js
@@ -1,17 +1,84 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 
-function createGrid(rows, columns) {
-for(r=0;r < rows * columns;r++) {
-let div = document.createElement('div');
-div.innerHTML = '';
-container.appendChild(div).className = "div-element";
-}
-}
+createGrid(16);
+draw();
+resetGrid();
+resizeGrid();
 
-createGrid(16, 16);
+let drawButton = document.querySelector('.draw-button')
 
-let divElement = document.querySelector('.div-element')
-
-container.addEventListener('mouseover', e => {
-//code that changes white to black
+drawButton.addEventListener('click', e => {
+  draw();
 })
+
+let eraserButton = document.querySelector('.eraser-button')
+
+eraserButton.addEventListener('click', e => {
+  eraser();
+})
+
+
+function resetGrid() {
+  const resetGrid = document.querySelector('.reset-button')
+  resetGrid.addEventListener('click', e => {
+    const divElement = document.querySelectorAll('.div-element')
+  for(let i=0;i<divElement.length;i++) {
+    divElement[i].classList.remove('black')
+  }
+  })
+  }
+
+function createGrid(size) {
+  for(r=0;r < size * size;r++) {
+  let div = document.createElement('div');
+  div.innerHTML = '';
+  container.appendChild(div).className = "div-element";
+  }
+  container.style.gridTemplateColumns = `repeat(${size},1fr)`
+  container.style.gridTemplateRows = `repeat(${size},1fr)`
+  }
+  
+function draw() {
+  let grid = document.querySelectorAll('.div-element')
+  
+  for(let i=0;i<grid.length;i++) {
+    grid[i].addEventListener('click', e => {
+      console.log('working')
+      grid[i].classList.remove('white')
+      grid[i].classList.add('black')
+    })
+  }
+}
+
+function eraser() {
+  let grid = document.querySelectorAll('.div-element')
+  
+  for(let i=0;i<grid.length;i++) {
+    grid[i].addEventListener('click', e => {
+      grid[i].classList.remove('black')
+      grid[i].classList.add('white')
+    })
+  }
+}
+
+function resizeGrid() {
+  const gridSizeButton = document.querySelector('.grid-size')
+
+  gridSizeButton.addEventListener('click', e => {
+  const div = document.querySelectorAll('.div-element')
+  for(i=0;i<div.length;i++) {
+  container.removeChild(div[i])
+  }
+  let size = prompt('What size will the grid be?')
+  if(size < 4) {
+  alert('error too small')
+  createGrid(16)
+  } else if(size > 100) {
+  alert('error too big')
+  createGrid(16)
+  } else(createGrid(size))
+  draw();
+  })
+}
+
+
